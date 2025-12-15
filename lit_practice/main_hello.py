678c7ca5af91f5ad39ed280bfb56d31da370f57e
ppvid <ksm7848@gmail.com>
2025-12-15 15:43:14 +0900

    저장

--- lit_practice/main_hello.py
+++ lit_practice/main_hello.py
@@ -17,12 +17,17 @@ st.caption('C221011 김수민')
 '### 1. 뉴스 본문 키워드 빈도 시각화-Seaborn,Altair,Plotly 활용'
 st.caption('출처: 네이버 뉴스')
 tab1,tab2,tab3=st.tabs(['Seabron','Altair','Plotly'])
+from pathlib import Path
+import pandas as pd
+
+BASE_DIR = Path(__file__).resolve().parent   # 현재 main_hello.py가 있는 폴더
+df = pd.read_csv(BASE_DIR / "키워드카운트.csv")
+
 with tab1:
     st.caption('seaborn으로 그래프 그리기')
     import seaborn as sns
     import matplotlib.pyplot as plt
     import pandas as pd
-    df=pd.read_csv(r'lit_practice\키워드카운트.csv')
     plt.rcParams["font.family"] = "Malgun Gothic"
     fig,ax= plt.subplots(figsize=(6,4))
     sns.barplot(data=df,y='keyword',x='count')
@@ -64,15 +69,26 @@ with tab3:
 
 '## 2. 뉴스 제목 키워드 워드 클라우드'
 st.caption('출처: 네이버 뉴스')
-st.image('워드클라우드.png',use_container_width=True)
-'글로벌, 3개 부분, 마마 어워즈 및 여러 걸그룹이 언급되는 걸 보아 최근에 글로벌 시상식인 마마 어워즈에 나가 수상을 한 것이 이슈가 되었다. 또한 강감독과 매기강 등의 언급을 보면 케데헌의 감독의 역할이 주목받고 있음을 알 수 있다.'
+from pathlib import Path
 
-'## 3. 뉴스 본문 키워드 네트워크 시각화'
-st.caption('출처: 네이버 뉴스')
-'### 네트워크 시각화'
-st.image('네트워크그래프.png',use_container_width=True)
-'### circular 네트워크 시각화'
-st.image('circulara_네트워크.png',use_container_width=True)
+BASE_DIR = Path(__file__).resolve().parent  # main_hello.py가 있는 폴더 기준
+
+st.image(BASE_DIR / "워드클라우드.png", use_container_width=True)
+st.write(
+    "글로벌, 3개 부분, 마마 어워즈 및 여러 걸그룹이 언급되는 걸 보아 최근에 글로벌 시상식인 "
+    "마마 어워즈에 나가 수상을 한 것이 이슈가 되었다. 또한 강감독과 매기강 등의 언급을 보면 "
+    "케데헌의 감독의 역할이 주목받고 있음을 알 수 있다."
+)
+
+st.write("## 3. 뉴스 본문 키워드 네트워크 시각화")
+st.caption("출처: 네이버 뉴스")
+
+st.write("### 네트워크 시각화")
+st.image(BASE_DIR / "네트워크그래프.png", use_container_width=True)
+
+st.write("### circular 네트워크 시각화")
+st.image(BASE_DIR / "circulara_네트워크.png", use_container_width=True)
+
 '뉴스 내용에는 케이팝 데몬 헌터스의 설명인 넷플릭스 애니메이션 콘텐츠를 언급하고 있다. 골든, 차트, 트랙 사운드 등의 키워드를 보아 해당 영화의 ost가 많은 주목을 받고 있음을 알 수 있다.'
 st.divider()
 '## 추가 정보 및 게임'
